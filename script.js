9f2b7290fc4c92341d3d4e954f52478f89e7b2ae
Jennifer Sence <sencejenni@gmail.com>
2023-09-11 15:27:38 +0100

    added the variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const aceesKey = "l4cxO9gvmfeHi7BvRkwiJK74EgumvLsNJZ1FM_zGfEU";
+
+const formEl = document.querySelector("form");
+const searchInputEl = document.getElementById("search-input");
+const searchResultsEl = document.querySelector(".search-results");
+const showMoreButtonEl = document.getElementById("show-more-button");
